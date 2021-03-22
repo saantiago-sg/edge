1f50f83b97dd0fef3e060570802d520f601a9ac7
saantiago-sg <54604815+saantiago-sg@users.noreply.github.com>
2021-03-22 20:31:56 -0300

    pagina finalizada

--- edgebound/src/app/components/new-address/new-address.component.ts
+++ edgebound/src/app/components/new-address/new-address.component.ts
@@ -5,11 +5,44 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './new-address.component.html',
   styleUrls: ['./new-address.component.css']
 })
-export class NewAddressComponent implements OnInit {
+export class NewAddressComponent {
 
   constructor() { }
+// tslint:disable-next-line:no-inferrable-types
+items: any = ['0', '1', '2', '3', '4', '5'];
 
-  ngOnInit(): void {
-  }
+// tslint:disable-next-line:typedef
+searchActive(){
+const menuSearch = document.querySelector('.searchbox-mobile');
+menuSearch?.classList.add('searchbox-active');
+}
 
+// tslint:disable-next-line:typedef
+closeSearch(){
+  const menuSearch = document.querySelector('.searchbox-mobile');
+  menuSearch?.classList.remove('searchbox-active');
+}
+// tslint:disable-next-line:typedef
+showMenu(){
+  const navMobile = document.querySelector('.nav-mobile');
+  navMobile?.classList.add('active');
+}
+
+// tslint:disable-next-line:typedef
+volver(){
+  const navMobile = document.querySelector('.nav-mobile');
+  navMobile?.classList.remove('active');
+}
+
+// tslint:disable-next-line:typedef
+showSubs(){
+  const subsMobile = document.querySelector('.nav-mobile-subs');
+  subsMobile?.classList.toggle('active-subs');
+}
+
+// tslint:disable-next-line:typedef
+showSuggestions(){
+const suggetion = document.querySelector('.searchbox--suggestions');
+suggetion?.classList.toggle('show-suggetions');
+}
 }
